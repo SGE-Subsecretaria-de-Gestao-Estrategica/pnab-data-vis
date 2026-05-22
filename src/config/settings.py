1f101034d76210f3c051be448993e6b353391423
Gabriel Ribeiro Bizerril <gabrielribeirob.16@gmail.com>
2026-05-22 11:17:27 -0300

    Update settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -5,10 +5,12 @@ ROOT = Path(__file__).resolve().parents[2] # PASTA RAÍZ DO PROJETO
 
 DATA_PATH = ROOT / "data"
 
-DATA_SECTION1 = DATA_PATH / "section_1"
-DATA_SECTION2 = DATA_PATH / "section_2"
-DATA_SECTION3 = DATA_PATH / "section_3"
-DATA_SECTION4 = DATA_PATH / "section_4"
-DATA_SECTION5 = DATA_PATH / "section_5"
-DATA_SECTION6 = DATA_PATH / "section_6"
-DATA_SECTION7 = DATA_PATH / "section_7"
+DATA_PATH_SECTION1 = DATA_PATH / "section_1"
+DATA_PATH_SECTION2 = DATA_PATH / "section_2"
+DATA_PATH_SECTION3 = DATA_PATH / "section_3"
+DATA_PATH_SECTION4 = DATA_PATH / "section_4"
+DATA_PATH_SECTION5 = DATA_PATH / "section_5"
+DATA_PATH_SECTION6 = DATA_PATH / "section_6"
+DATA_PATH_SECTION7 = DATA_PATH / "section_7"
+
+CUBO_PATH = DATA_PATH / "input_data" / "dados_cubo_final_v0__2026-05-20_13-03.xlsx"

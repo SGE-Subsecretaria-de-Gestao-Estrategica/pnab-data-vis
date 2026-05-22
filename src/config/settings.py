a8116820d113552b6440c266261f9deac24b362f
Gabriel Ribeiro Bizerril <gabrielribeirob.16@gmail.com>
2026-05-22 10:33:38 -0300

    Create settings.py

---
+++ src/config/settings.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import os
+
+ROOT = Path(__file__).resolve().parents[2] # PASTA RAÍZ DO PROJETO
+
+DATA_PATH = ROOT / "data"
+
+DATA_SECTION1 = DATA_PATH / "section_1"
+DATA_SECTION2 = DATA_PATH / "section_2"
+DATA_SECTION3 = DATA_PATH / "section_3"
+DATA_SECTION4 = DATA_PATH / "section_4"
+DATA_SECTION5 = DATA_PATH / "section_5"
+DATA_SECTION6 = DATA_PATH / "section_6"
+DATA_SECTION7 = DATA_PATH / "section_7"
